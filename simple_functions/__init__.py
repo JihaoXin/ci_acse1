6ba30368cc2000bb13aab0dc213837d530753612
JihaoXin <46529913+JihaoXin@users.noreply.github.com>
2020-10-21 18:58:33 +0800

    Lint

--- simple_functions/__init__.py
+++ simple_functions/__init__.py
@@ -1,6 +1,6 @@
-from .functions1 import * # noqa
-from .constants import * #noqa
-from .print import *
+from .functions1 import my_sum, factorial
+from .constants import pi
+from .print import myprint
 from pkg_resources import get_distribution, DistributionNotFound
 try:
     __version__ = get_distribution(__name__).version
